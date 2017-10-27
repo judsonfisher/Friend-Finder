c7fc2d36ba63356a41ef3f602dfe4ba7bf021ef0
Your Namejudsonfisher <judsonefisher@gmail.com>
2017-10-27 15:50:03 -0500

    sorta functional

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friends = [
+	{
+		name: 'Judson',
+		q1: 3,
+		q2: 3,
+		q3: 3,
+		q4: 3,
+		q5: 3,
+		q6: 3,
+		q7: 3,
+		q8: 3,
+		q9: 3,
+		q10: 3
+	}
+
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -3,16 +3,13 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./app/routing/htmlRoutes.js')(app);
 require('./app/routing/apiRoutes.js')(app);
-
-
-
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
