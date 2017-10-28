fdf9b88c0eb5f3146e15d2ce6bd93e87b927d3ca
Your Namejudsonfisher <judsonefisher@gmail.com>
2017-10-28 15:06:46 -0500

    edits

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,19 @@
 var friends = [
 	{
-		name: 'Judson',
-		q1: 3,
-		q2: 3,
-		q3: 3,
-		q4: 3,
-		q5: 3,
-		q6: 3,
-		q7: 3,
-		q8: 3,
-		q9: 3,
-		q10: 3
+	  "name":"Judson",
+  	  "photo":"https://drive.google.com/file/d/0B1to1EBtRiiwUU02UDZpVUhNY1k/view?usp=sharing.jpg",
+  	  "scores":[
+	      3,
+	      3,
+	      3,
+	      3,
+	      3,
+	      3,
+	      3,
+	      3,
+	      3,
+	      3
+	    ]
 	}
 
 ];
